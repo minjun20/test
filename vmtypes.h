d5ce6678d18c1c4001f77914cb5c487f8da41205
minjun20 <112992121+minjun20@users.noreply.github.com>
2022-12-15 14:26:28 +0900

    Add files via upload

---
+++ vmtypes.h
@@ -0,0 +1,29 @@
+#ifndef VMTYPES_H_ 
+#define VMTYPES_H_
+
+typedef struct vmTable_t {
+    int *pageNumArr; 
+    int *frameNumArr; 
+    int *entryAgeArr; 
+    int length;
+    int pageFaultCount;
+    int tlbHitCount;
+    int tlbMissCount;
+} vmTable_t;
+
+vmTable_t* createVMtable(int length);
+
+void displayTable(vmTable_t** tableToView);
+
+void freeVMtable(vmTable_t** table);
+
+int** dramAllocate(int frameCount, int blockSize);
+
+void freeDRAM(int*** dblPtrArr, int frameCount);
+
+int getPageNumber(int mask, int value, int shift);
+
+int getOffset(int mask, int value);
+
+
+#endif 
